664e77fbb1acab4fbe556532314a9b6c732af7c2
Vedang Shivankar <142177991+VedangShivankar@users.noreply.github.com>
2024-09-29 12:08:16 -0400

    Add files via upload

---
+++ SAVED4.sql
@@ -0,0 +1,34 @@
+SET AUTOCOMMIT = OFF;
+
+COMMIT;
+ 
+ DELETE FROM employees;
+ SELECT * from employees;
+
+ROLLBACK;
+
+SELECT * FROM employees
+
+
+
+-- highlight each spaced line of code and run them in a sequence
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT * FROM employees
