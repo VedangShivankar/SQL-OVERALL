764b3242e1f6f9912312c8b2142aad7657df25a0
Vedang Shivankar <142177991+VedangShivankar@users.noreply.github.com>
2024-10-15 21:02:53 -0400

    Add files via upload

---
+++ SAVED12.sql
@@ -0,0 +1,23 @@
+INSERT INTO transactions (amount, customer_id)
+VALUES (1.00, NULL);
+SELECT * FROM transactions;
+
+INSERT INTO customers (first_name, last_name)
+VALUES ("Poppy", "Puff");
+SELECT * FROM customers;
+
+SELECT * 
+FROM transactions INNER JOIN customers
+ON transactions.customer_id = customers.customer_id;
+
+SELECT transaction_id, amount, first_name, last_name
+FROM transactions INNER JOIN customers
+ON transactions.customer_id = customers.customer_id;
+
+SELECT * 
+FROM transactions LEFT JOIN customers
+ON transactions.customer_id = customers.customer_id;
+
+SELECT * 
+FROM transactions RIGHT JOIN customers
+ON transactions.customer_id = customers.customer_id;
