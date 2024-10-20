2299b543d1073fddf3ccf5f3c2a6ea0464957a5c
Vedang Shivankar <142177991+VedangShivankar@users.noreply.github.com>
2024-10-20 13:15:33 -0400

    Add files via upload

---
+++ SAVED15.sql
@@ -0,0 +1,25 @@
+SELECT * FROM employees
+WHERE first_name LIKE "%s";
+
+SELECT * FROM employees
+WHERE last_name LIKE "%r";
+
+SELECT * FROM employees
+
+WHERE first_name LIKE "%s";
+	
+SELECT * FROM employees
+WHERE first_name LIKE "%sp";
+
+SELECT * FROM employees
+WHERE job LIKE "_ook";
+
+
+SELECT * FROM employees
+WHERE hire_date LIKE "_____-01-__";
+
+SELECT * FROM employees
+WHERE hire_date LIKE "_____-__-02";
+
+SELECT * FROM employees
+WHERE job LIKE "_a%";
