2176bed0cb1df0e6471d8d534e1efa7f6e9ba6f8
Vedang Shivankar <142177991+VedangShivankar@users.noreply.github.com>
2024-10-21 18:27:20 -0400

    Add files via upload

---
+++ SAVED16.sql
@@ -0,0 +1,11 @@
+SELECT * FROM  employees
+ORDER BY last_name DESC;
+
+SELECT * FROM  employees
+ORDER BY first_name DESC;
+
+SELECT * FROM  employees
+ORDER BY hire_date;
+
+SELECT * FROM transactions
+ORDER BY amount, customer_id DESC;
