d540d3c37e1d20dd0c256fd1feec1f9f4b377ed6
Vedang Shivankar <142177991+VedangShivankar@users.noreply.github.com>
2024-10-22 16:38:29 -0400

    Add files via upload

---
+++ SAVED17.sql
@@ -0,0 +1,11 @@
+SELECT * FROM customers
+LIMIT 1;
+
+SELECT * FROM customers
+ORDER BY last_name LIMIT 3;
+
+SELECT * FROM customers
+ORDER BY last_name DESC LIMIT 2;
+
+SELECT * FROM customers
+LIMIT 2, 1;
